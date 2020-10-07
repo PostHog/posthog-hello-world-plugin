abdaa49bcdd7a5cdad956a7addd808631b7ec073
James Greenhill <fuziontech@gmail.com>
2020-10-07 10:58:29 +0000

    add requirements and base class

---
+++ __init__.py
@@ -0,0 +1,13 @@
+class Plugin(ABC):
+
+    @abstractmethod
+    def process_event(self, event):
+        pass
+
+    @abstractmethod
+    def process_person(self, event):
+        pass
+
+    @abstractmethod
+    def process_identify(self, event):
+        pass
