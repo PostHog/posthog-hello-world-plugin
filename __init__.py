ae91dc6b5dcce3083091bf24d43218c9dc4bd2c9
James Greenhill <fuziontech@gmail.com>
2020-10-08 11:43:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@ from posthog.plugins import PluginBaseClass, PosthogEvent
 
 class ExamplePlugin(PluginBaseClass):
     def __init__(self, config):
+        self.bar = config['foo']
         pass
     
     def process_event(self, event: PosthogEvent):
         event.properties["hello"] = "world"
+        event.properties["bar"] = self.bar
         return event
