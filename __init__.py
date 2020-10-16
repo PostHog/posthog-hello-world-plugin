bdabcd87505db2fc714646e729c297b827667f84
Marius Andra <marius.andra@gmail.com>
2020-10-16 16:37:08 +0200

    init syntax

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 from posthog.plugins import PluginBaseClass, PosthogEvent
 
-1 / 0
-raise Exception("bla")
+
 class ExamplePlugin(PluginBaseClass):
     def process_event(self, event: PosthogEvent):
-        event.properties["hello"] = "world"
-        event.properties["bar"] = self.config["bar"]
-        return event
+    event.properties["hello"] = "world"
+    event.properties["bar"] = self.config["bar"]
+    return event
