2dfff8b5d6053d815ab4b555b1be4282857bd2a4
Marius Andra <marius.andra@gmail.com>
2020-10-16 16:36:17 +0200

    no __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from posthog.plugins import PluginBaseClass, PosthogEvent
-
-
-class ExamplePlugin(PluginBaseClass):   
-    def process_event(self, event: PosthogEvent):
-        event.properties["hello"] = "world"
-        event.properties["bar"] = self.config["bar"]
-        return event
