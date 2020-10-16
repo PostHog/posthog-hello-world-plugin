bd5bb06101606953e36caa4cbd9de7b7b39e7f89
Marius Andra <marius.andra@gmail.com>
2020-10-16 16:38:53 +0200

    test instance and team init

--- __init__.py
+++ __init__.py
@@ -2,7 +2,14 @@ from posthog.plugins import PluginBaseClass, PosthogEvent
 
 
 class ExamplePlugin(PluginBaseClass):
+    @staticmethod
+    def instance_init():
+        ExamplePlugin.hello = "world"
+
+    def team_init(self):
+        self.hello = "hello"
+
     def process_event(self, event: PosthogEvent):
-    event.properties["hello"] = "world"
-    event.properties["bar"] = self.config["bar"]
-    return event
+        event.properties["instance"] = ExamplePlugin.hello
+        event.properties["team"] = self.hello
+        return event
