241d6f5035f4cf8e533a417cdd526e46a8c69b62
Marius Andra <marius.andra@gmail.com>
2020-10-16 16:40:08 +0200

    raise instance init

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@ from posthog.plugins import PluginBaseClass, PosthogEvent
 
 
 class ExamplePlugin(PluginBaseClass):
-    def process_event(self, event: PosthogEvent):
-        raise Exception("this is fine")
+    @staticmethod
+    def instance_init():
+        raise Exception("Something fishy")
