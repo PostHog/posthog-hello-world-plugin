64f428010a1c850f0a0764b95ed439c985a72d12
Marius Andra <marius.andra@gmail.com>
2020-10-16 16:39:20 +0200

    raise in event

--- __init__.py
+++ __init__.py
@@ -2,14 +2,5 @@ from posthog.plugins import PluginBaseClass, PosthogEvent
 
 
 class ExamplePlugin(PluginBaseClass):
-    @staticmethod
-    def instance_init():
-        ExamplePlugin.hello = "world"
-
-    def team_init(self):
-        self.hello = "hello"
-
     def process_event(self, event: PosthogEvent):
-        event.properties["instance"] = ExamplePlugin.hello
-        event.properties["team"] = self.hello
-        return event
+        raise Exception("this is fine")
