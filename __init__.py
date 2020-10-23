547bf48f3434ccfd4bed464cdf2cf629485a0cc6
Marius Andra <marius.andra@gmail.com>
2020-10-23 09:37:39 +0200

    extract jsplugin class and convert to syntax that uses global functions instead of initializing a class

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from posthog.plugins import PluginBaseClass, PosthogEvent
-
-
-class ExamplePlugin(PluginBaseClass):
-    def team_init(self):
-        raise Exception("Something fishy in this team")
